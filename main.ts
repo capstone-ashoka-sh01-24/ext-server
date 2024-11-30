bf3e3e22752823712069bdd088b7c678c5516dfb
shivamkedia17 <65805659+shivamkedia17@users.noreply.github.com>
2024-11-30 08:12:11 +0530

    add: HTTP reponse codes; better error messages

--- main.ts
+++ main.ts
@@ -13,17 +13,21 @@ app.post("/", async (c) => {
     console.log("Set:", json);
 
     if (json.key === undefined || json.value === undefined) {
-      throw new Error("Missing field in key-value pair.");
+      c.status(400);
+      return c.json("Missing field in key-value pair.");
     }
     const { key, value } = json;
     const result = await store.set([key], value);
 
     if (result.ok) {
+      c.status(201);
       return c.json({ message: `Successfully Set: ${key}` });
     } else {
-      throw new Error(`Error while setting ${key}`);
+      c.status(500);
+      return c.json({ error: `Error while setting ${key}` });
     }
   } catch (e) {
+    c.status(500);
     return c.json({ error: `${e}` });
   }
 });
@@ -38,13 +42,24 @@ app.get("/:key", async (c) => {
     const result = await store.get([key]);
 
     if (result.value === null) {
-      return c.json({ error: `Key ${key} not found.` }, 404);
+      c.status(404);
+      return c.json({ error: `Key ${key} not found.` });
     } else {
+      c.status(200);
       return c.json({ value: result.value });
     }
   } catch (e) {
-    return c.json({ request: c.req.json(), error: `${e}` });
+    c.status(500);
+    return c.json({ error: `${e}` });
   }
+});
+
+app.get("/", (c) => {
+  c.status(400);
+  return c.json({
+    message:
+      "Only POST requests allowed on route /, for GET request use /<key>",
+  });
 });
 
 Deno.serve(app.fetch);
